c2bb6b07765e24b40133d020238cd9a4e1c9a963
Jaymes Rowley <36048273+EchoXTeknology@users.noreply.github.com>
2020-03-27 14:40:59 -0700

    Defines/Declares Various C++ Headers

---
+++ stdafx.hpp
@@ -0,0 +1,53 @@
+////////////////////////////////////////////////////////////
+//
+// File: stdafx.hpp
+// Copyright (C) 2018-2020 EchoXTeknology (lead@echoxteknology-dev.org)
+// Copyright (C) 2018-2020 EchoXStudios (echoxstudios@echoxteknology-dev.org)
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented;
+//    you must not claim that you wrote the original software.
+//    If you use this software in a product, an acknowledgment
+//    in the product documentation would be appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such,
+//    and must not be misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source distribution.
+//
+////////////////////////////////////////////////////////////
+
+
+
+#ifndef ECHOXSTUDIOS_STDAFX_HPP
+#define ECHOXSTUDIOS_STDAFX_HPP
+
+////////////////////////////////////////////////////////////
+// Pre-compiled System Headers
+////////////////////////////////////////////////////////////
+#include <Windows.h>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+
+
+////////////////////////////////////////////////////////////
+// Pre-compiled Handler Headers
+////////////////////////////////////////////////////////////
+#include <string>
+#include <vector>
+#include <array>
+#include <cmath>
+#include <list>
+
+
+
+#endif // !ECHOXSTUDIOS_STDAFX_HPP
